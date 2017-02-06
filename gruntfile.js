77fcbfbe3f886e8281b9ef1343047b35adafaa44
Irushka1982 <leshchyshyn.iryna@gmail.com>
2017-02-06 18:48:29 +0200

    create basis structure

---
+++ gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+    dist: {
+      files: [{
+        expand: true,
+        cwd: 'src/styles',
+        src: ['*.scss'],
+        dest: 'src/styles',
+        ext: '.css'
+      }]
+    }
+  },
+  
+  concat:{
+    dist:{
+      src:['src/styles/style.scss'],
+      dest:'src/styles/main.scss'
+    },
+   
+    dist:{
+      src:['src/js/script.js','src/js/vendor/bootstrap.js'],
+      dest:'public/mainscript.js'
+    }
+  },    
+
+  watch: {
+    sass: {
+      // We watch and compile sass files as normal but don't live reload here
+      files: ['**/*.scss'],
+      tasks: ['concat','sass'],
+    },
+  }
+  });
+  
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default',['sass'],['watch'],['concat']);
+
+};
---
+++ public/mainscript.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+       $(".bag__search-icon").click(function(e){
+          e.preventDefault();
+            $('.bag__search').show();
+          
+      });    
+
+});
